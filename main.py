8427b435cc228825cb9471a07fcd199792509575
byanko55 <byanko55@gmail.com>
2024-04-27 00:00:17 +0900

    connect DB

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/", response_class=HTMLResponse)
 async def index(request:Request):
     return templates.TemplateResponse("index.html", {"request":request, "msg":"Go"})
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
 
 @app.exception_handler(404)
 async def non_exist_page(request, __):
