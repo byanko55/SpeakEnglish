6d942b26a9f0f2b83fb093e0cd238fe6a4a7a7db
byanko55 <byanko55@gmail.com>
2024-05-06 09:18:27 +0900

    revise pagination class struct

--- static/main.js
+++ static/main.js
@@ -1,6 +1,82 @@
 const header = document.getElementById('header');
 const main = document.getElementById('main');
+const quizBox = document.querySelector('.quizbox');
+const tablebox = document.querySelector('.tablebox');
+const tabMain = document.getElementById('link-main');
+const tabList = document.getElementById('link-list');
+const recordsPerPage = 15;
+
 let pagination;
+const rootURL = (window.location.href).split("?")[0];
+
+
+class Pagination{
+    constructor(page, keyword, limit = 15) {
+        this.currentPage = (page == null) ? 1 : page;
+        this.filterKeyword = (keyword == null) ? '' : keyword;
+        this.paginationLimit = limit;
+        this.pageCount = 0;
+        this.reload = false;
+
+        document.getElementById("input-page").value = this.currentPage;
+        getPageCount();
+    }
+
+    disableButton(button){
+        button.classList.remove("active");
+    }
+
+    enableButton(button){
+        button.classList.add("active");
+    }
+
+    initPage(){
+        this.setCurrentPage(this.currentPage);
+    }
+    
+    setCurrentPage(page){
+        if (! isInteger(page)
+        || page < 1
+        || page > this.pageCount
+        ){
+            window.location.replace("404.html");
+        }
+
+        this.currentPage = page;
+        
+        const nextButton = document.getElementById("btn-nextpage");
+        const prevButton = document.getElementById("btn-prevpage");
+        const firstButton = document.getElementById("btn-firstpage");
+        const lastButton = document.getElementById("btn-lastpage");
+
+        if (this.currentPage == 1) {
+            this.disableButton(prevButton);
+            this.disableButton(firstButton);
+        } else {
+            this.enableButton(prevButton);
+            this.enableButton(firstButton);
+        }
+    
+        if (this.pageCount == this.currentPage) {
+            this.disableButton(nextButton);
+            this.disableButton(lastButton);
+        } else {
+            this.enableButton(nextButton);
+            this.enableButton(lastButton);
+        }
+    }
+}
+
+
+const isInteger = (v) => {
+    if (Number.isInteger(v)) return true;
+
+    if (typeof v !== 'string') return false;
+
+    var n = Math.floor(Number(v));
+
+    return n !== Infinity && String(n) === v;
+}
 
 
 const revealAnswer = () => {
@@ -54,12 +130,82 @@ const nextQuestion = () => {
 };
 
 
+const refreshMainUI = () => {
+    quizBox.classList.add('active');
+    tabMain.classList.add('active');
+    tablebox.classList.remove('active');
+    tabList.classList.remove('active');
+
+    window.history.replaceState(null, null, rootURL);
+};
+
+
+const refreshListUI = () => {
+    quizBox.classList.remove('active');
+    tabMain.classList.remove('active');
+    tablebox.classList.add('active');
+    tabList.classList.add('active');
+
+    newURL = new URL(rootURL);
+    newURL.searchParams.set('page', pagination.currentPage);
+
+    if (pagination.filterKeyword != '') {
+        newURL.searchParams.set('keyword', pagination.filterKeyword);
+    }
+
+    window.history.replaceState(null, null, newURL);
+
+    if (pagination.reload == false) {
+        getSentenceList(pagination.currentPage);
+        pagination.reload = true;
+    }
+};
+
+
+const getPageCount = () => {
+    var res;
+
+    $.getJSON('/count', {keyword:this.filterKeyword}, function (data) {
+        console.log("[getPageCount] Get the number of sentences");
+        res = data[0];
+    })
+    .done(function() {
+        let pageCount = Math.ceil(res.count / pagination.paginationLimit);
+        pagination.pageCount = pageCount;
+        pagination.initPage();
+
+        document.querySelector(".total-pages").innerText = pageCount;
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) { console.log('[getPageCount] getJSON request failed: ' + textStatus); })
+    .always(function() { console.log('[getPageCount] getJSON request ended!'); });
+}
+
+
 const resetPage = () => {
     let btnReset = document.getElementById('btn-reset');
 
     btnReset.addEventListener('click', function() {
-        initPagination(1, 15);
-        getSentenceList(1);
+        pagination = new Pagination(1, '', recordsPerPage);
+        refreshListUI();
+    });
+}
+
+
+const hideBackground = () => {
+    main.classList.add('transparent');
+}
+
+
+const showBackground = () => {
+    main.classList.remove('transparent');
+}
+
+
+const addNewSentence = () => {
+    let btnAdd = document.getElementById('btn-add');
+
+    btnAdd.addEventListener('click', function() {
+        hideBackground();
     });
 }
 
@@ -114,97 +260,8 @@ const getSentenceList = (page) => {
 }
 
 
-const initPagination = (page, limit) => {
-    var res;
-
-    $.getJSON('/count', {}, function (data) {
-        console.log("[initPagination] Get the number of sentences");
-        res = data[0];
-    })
-    .done(function() {
-        pagination = new Pagination(page, limit, res.count);
-        pagination.setCurrentPage(page);
-
-        const url = new URL(window.location.href);
-        url.searchParams.set('page', page);
-
-        window.history.replaceState(null, null, url);
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) { console.log('[initPagination] getJSON request failed: ' + textStatus); })
-    .always(function() { console.log('[initPagination] getJSON request ended!'); });
-};
-
-
-class Pagination{
-    constructor(page, limit, numRecords) {
-        this.currentPage = page;
-        this.paginationLimit = limit;
-        this.pageCount = Math.ceil(numRecords / limit);
-
-        document.getElementById("input-page").value = page;
-        document.querySelector(".total-pages").innerText = this.pageCount;
-    }
-
-    disableButton(button){
-        button.classList.remove("active");
-    };
-    
-    enableButton(button){
-        button.classList.add("active");
-    };
-    
-    setCurrentPage(page){
-        this.currentPage = page;
-        
-        const nextButton = document.getElementById("btn-nextpage");
-        const prevButton = document.getElementById("btn-prevpage");
-        const firstButton = document.getElementById("btn-firstpage");
-        const lastButton = document.getElementById("btn-lastpage");
-
-        if (this.currentPage == 1) {
-            this.disableButton(prevButton);
-            this.disableButton(firstButton);
-        } else {
-            this.enableButton(prevButton);
-            this.enableButton(firstButton);
-        }
-    
-        if (this.pageCount == this.currentPage) {
-            this.disableButton(nextButton);
-            this.disableButton(lastButton);
-        } else {
-            this.enableButton(nextButton);
-            this.enableButton(lastButton);
-        }
-    };
-}
-
-
 document.addEventListener('DOMContentLoaded', function(){
-    let quizBox = document.querySelector('.quizbox');
-    let tablebox = document.querySelector('.tablebox');
-
-    let lastPage = 1;
-    let lastKeyword = '';
-
     // tab
-    let tabMain = document.getElementById('link-main');
-    let tabList = document.getElementById('link-list');
-
-    const refreshMainUI = () => {
-        quizBox.classList.add('active');
-        tabMain.classList.add('active');
-        tablebox.classList.remove('active');
-        tabList.classList.remove('active');
-    };
-    
-    const refreshListUI = () => {
-        quizBox.classList.remove('active');
-        tabMain.classList.remove('active');
-        tablebox.classList.add('active');
-        tabList.classList.add('active');
-    };
-
     tabMain.addEventListener('click', function() {
         refreshMainUI();
     });
@@ -213,15 +270,16 @@ document.addEventListener('DOMContentLoaded', function(){
         refreshListUI();
     });
 
+
     // URL params
     const urlParams = new URLSearchParams(window.location.search);
+    let ArgPage = urlParams.get('page');
+    let ArgKeyword = urlParams.get('keyword');
 
-    if (urlParams.has('page')){
+    pagination = new Pagination(ArgPage, ArgKeyword);
+
+    if (ArgPage != null) {
         refreshListUI();
-
-        lastPage = urlParams.get('page');
-        getSentenceList(lastPage);
-        initPagination(lastPage, 15);
     }
 
     // Reveal
@@ -236,6 +294,7 @@ document.addEventListener('DOMContentLoaded', function(){
     resetPage();
 
     // Add
+    addNewSentence();
 
     // Search
 
